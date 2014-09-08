62942e154981c4187ec8486fc960efc5eb9e27ba
chadmichel <chadmichel@me.com>
2014-09-07 22:54:19 -0500

    remove unneeded list

--- app/js/app.js
+++ app/js/app.js
@@ -5,15 +5,11 @@ var noteApp = angular.module('noteApp', [
 noteApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/list', {
-        templateUrl: 'app/partials/note-list.html',
-        controller: 'ListCtrl'
-      }).
       when('/note/:id', {
         templateUrl: 'app/partials/note-details.html',
         controller: 'DetailsCtrl'
       }).
       otherwise({
-        redirectTo: '/list'
+        redirectTo: '/note/new'
       });
   }]);
