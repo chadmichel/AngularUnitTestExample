c05db57412f178f5cbad00cdddc609ecc5a93f34
chadmichel <chadmichel@me.com>
2014-09-07 23:20:43 -0500

    add sync configs services

---
+++ app/js/services/syncconfigs.js
@@ -0,0 +1,12 @@
+
+angular.module("noteApp").service('syncConfigs', ['$rootScope', '$q', 'guid', function($rootScope, $q, guid) {
+
+    function SyncConfigs() {
+
+        var self = this;        
+    }
+
+    var result = new syncConfigs();
+    return result;
+
+}]);
